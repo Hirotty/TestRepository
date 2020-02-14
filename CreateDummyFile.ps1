5f47bdb7d6d5ceb3bd785d440e4a37a57f58cae2
Hiroshi Yamamoto <vsn_2255903@i.softbank.jp>
2020-02-14 17:46:16 +0900

    Add PowerShell Files

---
+++ CreateDummyFile.ps1
@@ -0,0 +1,9 @@
+﻿Param(
+    # カレントディレクトリのパス
+    [parameter(mandatory=$true)][String]$strCurrentDirectory
+)
+[String]$strJsonFile = "DummyFileIni.json"
+
+$strCurrentDirectory = $strCurrentDirectory + "\" + $strJsonFile
+$conf = Get-Content $strCurrentDirectory -Encoding UTF8 -Raw | ConvertFrom-Json
+
